54687ab0e1b14d0399307fe64782091763c6e74f
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-03-02 18:50:40 +0100

    lade till id till buttonsen för att javascript ska identifiera

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function()){
    const buttons = document.querySelectorAll('.flex-conteiner button');
-
+buttons.forEach(button, index) {
+button.addEventListener('click', function() {
+    const article = button.closest('article');
+    const imgsSrc
+}
+}
 
 
 
